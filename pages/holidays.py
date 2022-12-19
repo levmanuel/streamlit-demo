a102d33a4c756f75897c24b2d3de636c33ff12c6
levmanuel <manuel.leveque@gmail.com>
2022-12-19 13:46:33 +0100

    Update holidays.py

--- pages/holidays.py
+++ pages/holidays.py
@@ -20,7 +20,7 @@ coordinates = [[59.91512811000568, 10.739105977295354], [60.46905260044231, 5.37
 folium.PolyLine(coordinates, color="red", weight=2.5, opacity=1).add_to(m)
 
 # call to render Folium map in Streamlit
-#
+# st_data = st_folium(m, width=725)
 
 #Dataframe
 nof = pd.DataFrame({
@@ -28,12 +28,9 @@ nof = pd.DataFrame({
 "start": [45, 0.75*45, 25, min(0.75*45, 25), 20],
 "updated": [45, 0.75*45, 25, min(0.75*45, 25), 20]})
 
+col1, col2 = st.columns(2)
 with c1:
     st.df(nof)
 
 with c2:
     st_data = st_folium(m, width=725)
-
-col1, col2 = st.columns(2)
-col1.dataframe(nof)
-col2.st_folium(m, width=725)
