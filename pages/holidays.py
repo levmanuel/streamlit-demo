cc68399a289e99f7b1dd7aa83c10a28afae78c0f
levmanuel <manuel.leveque@gmail.com>
2022-12-19 13:11:36 +0100

    Update holidays.py

--- pages/holidays.py
+++ pages/holidays.py
@@ -8,7 +8,7 @@ m = folium.Map(location=[59.91512811000568, 10.739105977295354], zoom_start=16)
 folium.Marker([59.91512811000568, 10.739105977295354], popup="Oslo", tooltip="Oslo").add_to(m)
   
 # Create a list of coordinates representing the points along the route
-coordinates = [[59.91512811000568, 10.739105977295354], [60.46905260044231, 5.371412422001533]
+coordinates = [[59.91512811000568, 10.739105977295354], [60.46905260044231, 5.371412422001533]]
 
 # Add the itinerary to the map as a PolyLine
 #folium.PolyLine(coordinates, color="red", weight=2.5, opacity=1).add_to(m)
