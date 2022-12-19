fcea053b63c04e53958805fb006c3ac9849860c4
levmanuel <manuel.leveque@gmail.com>
2022-12-19 13:38:53 +0100

    Update holidays.py

--- pages/holidays.py
+++ pages/holidays.py
@@ -5,6 +5,9 @@ from streamlit_folium import st_folium
 
 st.set_page_config(page_title="Holidays in Norway", page_icon="✈️")
 
+st.title("Holidays in Norway ✈️")
+st.write("Day-to-Day Planner")
+
 # center on Oslo
 m = folium.Map(location=[59.91512811000568, 10.739105977295354], zoom_start=8)
 folium.Marker([59.91512811000568, 10.739105977295354], popup="Oslo", tooltip="Oslo").add_to(m)
