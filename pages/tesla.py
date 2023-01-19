6c46fbd4cdde82136d6a62f5adc43a52527ddaca
levmanuel <manuel.leveque@gmail.com>
2023-01-19 12:49:35 +0100

    Update tesla.py

--- pages/tesla.py
+++ pages/tesla.py
@@ -4,3 +4,12 @@
 ## cout de recharge
 #- cout de recharge à la maison
 ## planification de voyage
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.set_page_config(page_title="Model 3 SR+ Overview", page_icon="🚗")
+
+st.title("Model 3 SR+ Overview")
+st.write("test")
