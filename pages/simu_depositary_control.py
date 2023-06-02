07f0a71e08db6744ce43e6475187d151daf0f2ac
levmanuel <manuel.leveque@gmail.com>
2023-06-02 22:29:51 +0200

    Update simu_depositary_control.py

--- pages/simu_depositary_control.py
+++ pages/simu_depositary_control.py
@@ -2,35 +2,31 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
 st.set_page_config(page_title="Depositary Control Data Science Project", page_icon="🐶")
-
 st.title("Depositary Control Data Science Project")
 
 col1, col2 = st.columns(2)
 with col1:
+    
+    st.title("Transaction Input")
     booking_date = st.date_input('Booking Date')
     st.write("Vous avez sélectionné la date de réservation: ", booking_date)
 
-value_date = st.date_input('Value Date')
-st.write("Vous avez sélectionné la date de valeur: ", value_date)
+    value_date = st.date_input('Value Date')
+    st.write("Vous avez sélectionné la date de valeur: ", value_date)
 
-description = st.text_input('Description')
-st.write("Vous avez entré la description: ", description)
+    description = st.text_input('Description')
+    st.write("Vous avez entré la description: ", description)
 
-net_amount = st.number_input('Net Amount', value=0.00)
-st.write("Vous avez entré le montant net: ", net_amount)
+    net_amount = st.number_input('Net Amount', value=0.00)
+    st.write("Vous avez entré le montant net: ", net_amount)
 
-market_value = st.number_input('Fund Market Value', value=0.00)
-st.write("Vous avez entré la valeur du marché: ", market_value)
+    market_value = st.number_input('Fund Market Value', value=0.00)
+    st.write("Vous avez entré la valeur du marché: ", market_value)
 
 
-
+with col2:
+    st.title("Transaction Summary")
 
 if st.button('Submit'):
     st.write("Données soumises avec succès")
-
-with col2:
-    st_data = st_folium(m)
-
-
