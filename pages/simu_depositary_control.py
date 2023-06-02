21c6823b583022d8161ad01d7c3210b7b549ba2a
levmanuel <manuel.leveque@gmail.com>
2023-06-02 22:50:41 +0200

    Update simu_depositary_control.py

--- pages/simu_depositary_control.py
+++ pages/simu_depositary_control.py
@@ -2,6 +2,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from datetime import date, timedelta
+
 st.set_page_config(page_title="Depositary Control Data Science Project", page_icon="🐶")
 st.title("Depositary Control Data Science Project")
 
@@ -15,13 +17,13 @@ with col1:
     value_date = st.date_input('Value Date')
     st.write("Vous avez sélectionné la date de valeur: ", value_date)
 
-    description = st.text_input('Description')
+    description = st.text_input(' XYZ')
     st.write("Vous avez entré la description: ", description)
 
-    net_amount = st.number_input('Net Amount', value=0.00)
+    net_amount = st.number_input('Net Amount', value= 300_000 )
     st.write("Vous avez entré le montant net: ", net_amount)
 
-    market_value = st.number_input('Fund Market Value', value=0.00)
+    market_value = st.number_input('Fund Market Value', value= 10_000_000 )
     st.write("Vous avez entré la valeur du marché: ", market_value)
 
 
@@ -30,6 +32,7 @@ with col2:
     data_dict = {'Booking Date': [booking_date], 'Value Date': [value_date], 
                  'Description': [description], 'net_amount': [net_amount], "market_value": [market_value]}
     df = pd.DataFrame(data_dict)
+    df["NAV_pct"] = 100 * df["net_amount"] / df["market_value"]
     st.dataframe(df.transpose())
 
 if st.button('Submit'):
