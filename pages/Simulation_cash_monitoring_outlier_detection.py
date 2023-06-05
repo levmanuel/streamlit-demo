629282b9826480e2fb00dad29294c6bb52127beb
levmanuel <manuel.leveque@gmail.com>
2023-06-05 16:25:40 +0200

    Update Simulation_cash_monitoring_outlier_detection.py

--- pages/Simulation_cash_monitoring_outlier_detection.py
+++ pages/Simulation_cash_monitoring_outlier_detection.py
@@ -12,19 +12,19 @@ with col1:
     
     st.write("Transaction Input")
     booking_date = st.date_input('Booking Date')
-    st.write("Vous avez sélectionné la date de réservation: ", booking_date)
+    #st.write("Vous avez sélectionné la date de réservation: ", booking_date)
 
     value_date = st.date_input('Value Date')
-    st.write("Vous avez sélectionné la date de valeur: ", value_date)
+    #st.write("Vous avez sélectionné la date de valeur: ", value_date)
 
     description = st.text_input(' Description')
-    st.write("Vous avez entré la description: ", description)
+    #st.write("Vous avez entré la description: ", description)
 
     net_amount = st.number_input('Net Amount', value= 300_000 )
-    st.write("Vous avez entré le montant net: ", net_amount)
+    #st.write("Vous avez entré le montant net: ", net_amount)
 
     market_value = st.number_input('Fund Market Value', value= 10_000_000 )
-    st.write("Vous avez entré la valeur du marché: ", market_value)
+    #st.write("Vous avez entré la valeur du marché: ", market_value)
 
 
 with col2:
