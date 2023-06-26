f2621fd3c1b504e36bcc2c9cc0c9bdb94064e496
levmanuel <manuel.leveque@gmail.com>
2023-06-27 00:27:51 +0200

    ok

--- pages/Simulation_cash_monitoring_outlier_detection.py
+++ pages/Simulation_cash_monitoring_outlier_detection.py
@@ -102,8 +102,6 @@ def extract_alpha_sequences(string, max_word=None):
         matches = re.sub(regex, '[CCY]', matches)
         return matches
 
-
-
 bad_words = ["fraud",
     "suspicious",
     "unauthorized",
@@ -205,4 +203,4 @@ def has_bad_words(string):
         return 0
     
 st.write(extract_alpha_sequences(description,15))
-st.write(has_bad_words(description))
+st.write(has_bad_words(extract_alpha_sequences(description,15)))
