305c7ae64bf15b4260499afc5e7c0ebf9a0d92b0
levmanuel <manuel.leveque@gmail.com>
2023-06-26 21:01:47 +0200

    Update Simulation_cash_monitoring_outlier_detection.py

--- pages/Simulation_cash_monitoring_outlier_detection.py
+++ pages/Simulation_cash_monitoring_outlier_detection.py
@@ -37,3 +37,51 @@ with col2:
 
 if st.button('Submit'):
     st.write("Données soumises avec succès")
+
+
+# Champ 'is_opp_transaction_0' et 'is_opp_transaction_1'
+is_opp_transaction_0 = st.checkbox("is_opp_transaction_0")
+is_opp_transaction_1 = st.checkbox("is_opp_transaction_1")
+
+# Champ 'is_3_sigma_0' et 'is_3_sigma_1'
+is_3_sigma_0 = st.checkbox("is_3_sigma_0")
+is_3_sigma_1 = st.checkbox("is_3_sigma_1")
+
+# Champ 'date_delta_7_0' et 'date_delta_7_1'
+date_delta_7_0 = st.checkbox("date_delta_7_0")
+date_delta_7_1 = st.checkbox("date_delta_7_1")
+
+# Champ 'date_delta_30_0' et 'date_delta_30_1'
+date_delta_30_0 = st.checkbox("date_delta_30_0")
+date_delta_30_1 = st.checkbox("date_delta_30_1")
+
+# Champ 'fin_semaine_0' et 'fin_semaine_1'
+fin_semaine_0 = st.checkbox("fin_semaine_0")
+fin_semaine_1 = st.checkbox("fin_semaine_1")
+
+# Champ 'fin_mois_0' et 'fin_mois_1'
+fin_mois_0 = st.checkbox("fin_mois_0")
+fin_mois_1 = st.checkbox("fin_mois_1")
+
+# Champ 'fin_trimestre_0' et 'fin_trimestre_1'
+fin_trimestre_0 = st.checkbox("fin_trimestre_0")
+fin_trimestre_1 = st.checkbox("fin_trimestre_1")
+
+# Champ 'fin_semestre_0' et 'fin_semestre_1'
+fin_semestre_0 = st.checkbox("fin_semestre_0")
+fin_semestre_1 = st.checkbox("fin_semestre_1")
+
+# Champ 'fin_annee_0' et 'fin_annee_1'
+fin_annee_0 = st.checkbox("fin_annee_0")
+fin_annee_1 = st.checkbox("fin_annee_1")
+
+# Champ 'is_similar_0' et 'is_similar_1'
+is_similar_0 = st.checkbox("is_similar_0")
+is_similar_1 = st.checkbox("is_similar_1")
+
+# Champ 'has_bad_word'
+has_bad_word = st.checkbox("has_bad_word")
+
+# Champ: deal_type_0, deal_type_1, deal_type_2, deal_type_3, deal_type_4, deal_type_5
+deal_types = ['deal_type_0', 'deal_type_1', 'deal_type_2', 'deal_type_3', 'deal_type_4', 'deal_type_5']
+selected_deal_type = st.selectbox("Select Deal Type", deal_types)
