c2beb3e020dd11dcdbd1cd5cef3ddecf7fa5e877
levmanuel <manuel.leveque@gmail.com>
2023-06-26 21:35:16 +0200

    Update Simulation_cash_monitoring_outlier_detection.py

--- pages/Simulation_cash_monitoring_outlier_detection.py
+++ pages/Simulation_cash_monitoring_outlier_detection.py
@@ -5,7 +5,7 @@ import numpy as np
 from datetime import date, timedelta
 import calendar
 
-st.set_page_config(page_title="cas Monitoring Outlier Detection", page_icon="🐶")
+st.set_page_config(page_title="Cash Monitoring Outlier Detection", page_icon="🐶")
 st.title("Depositary Control Data Science Project")
 
 col1, col2 = st.columns(2)
