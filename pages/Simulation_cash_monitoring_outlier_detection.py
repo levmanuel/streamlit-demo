4e3769e2d6966833d7d6eaf41d195b652155888e
levmanuel <manuel.leveque@gmail.com>
2023-06-26 21:43:23 +0200

    Update Simulation_cash_monitoring_outlier_detection.py

--- pages/Simulation_cash_monitoring_outlier_detection.py
+++ pages/Simulation_cash_monitoring_outlier_detection.py
@@ -17,13 +17,22 @@ with col1:
     net_amount = st.number_input('Net Amount', value= 300_000 )
     market_value = st.number_input('Fund Market Value', value= 10_000_000 )
     date_delta =  (value_date - booking_date).days
+    date_delta_7 = np.where(np.abs(df["date_delta"])> 7,1,0)
+    date_delta_30 = np.where(np.abs(df["date_delta"])> 30,1,0)
+    fin_semaine = df["Booking Date"].apply(lambda x: (x.weekday() >= 5)).astype(int)
+    df['fin_mois'] = df['Booking Date'].apply(lambda x: (x.day >= calendar.monthrange(x.year, x.month)[1]-5)).astype(int)
+    df['fin_trimestre'] = df['Booking Date'].apply(lambda x: (x.day >= calendar.monthrange(x.year, ((x.month-1)//3+1)*3)[1]-5)).astype(int)
+    df['fin_semestre'] = df['Booking Date'].apply(lambda x: (x.day >= calendar.monthrange(x.year, ((x.month-1)//6+1)*6)[1]-5)).astype(int)
+    df['fin_annee'] = df['Booking Date'].apply(lambda x: (x.month == 12 and x.day >= 26)).astype(int)
 
 with col2:
     st.write("Transaction Summary")
     data_dict = {
         'Booking Date': [booking_date], 'Value Date': [value_date], 
         'Description': [description], 'net_amount': [net_amount], 
-        'market_value': [market_value], 'date_delta' : [date_delta] }
+        'market_value': [market_value], 'date_delta' : [date_delta],
+        'date_delta_7' : [date_delta_7], 'date_delta_30' : [date_delta_30],
+        'fin_semaine' : [fin_semaine]}
     df = pd.DataFrame(data_dict)
     df["NAV_pct"] = 100 * df["net_amount"] / df["market_value"]
     st.dataframe(df.transpose())
