4952ea8a6a89485056af65c640998b599dffbfbf
levmanuel <manuel.leveque@gmail.com>
2023-06-27 23:53:33 +0200

    test

--- file_upload.py
+++ file_upload.py
@@ -127,13 +127,8 @@ def has_bad_words(string):
         else:
             return 0
         
-
 with open('kmeans_nlp.pkl', 'rb') as fichier:
     kmeans_charge = pickle.load(fichier)
 
 with open('count_vectorizer.pkl', 'rb') as fichier:
     C_V_charge = pickle.load(fichier)
-
-
-with open('isolation_forest_nlp.pkl', 'rb') as fichier:
-    nlp_Iso_Forest_charge = pickle.load(fichier)
