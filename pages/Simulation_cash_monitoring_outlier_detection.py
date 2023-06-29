aeebc4b4508d7d4b534c9d3c03ca381917f8efdf
levmanuel <manuel.leveque@gmail.com>
2023-06-29 22:20:12 +0200

    ok

--- pages/Simulation_cash_monitoring_outlier_detection.py
+++ pages/Simulation_cash_monitoring_outlier_detection.py
@@ -57,7 +57,7 @@ with col2:
     
     df = pd.DataFrame(data_dict)
     nav_pct = 100 * df["net_amount"] / df["market_value"]
-    df["nav_pct"] = nav_pct
+    df["nav_pct"] = nav_pct.values
     st.dataframe(df.transpose(),  width = 800)
 
     st.write("Date features")
