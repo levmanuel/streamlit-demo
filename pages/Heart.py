ced8b340f3c633c8cfd5532d6a5213da94ee77bd
levmanuel <manuel.leveque@gmail.com>
2024-11-06 22:21:25 +0100

    OK

--- pages/Heart.py
+++ pages/Heart.py
@@ -3,7 +3,7 @@ import streamlit as st
 def generate_heart(text):
     # Génère le motif en forme de cœur avec le texte fourni
     heart_pattern = '\n'.join(
-        [''.join(
+        [(' ' * (30 - y // 2)) + ''.join(  # Ajout de l'espacement en fonction de y pour centrer chaque ligne
             [(text[(x - y) % len(text)]
               if ((x * 0.05) ** 2 + (y * 0.1) ** 2 - 1) ** 3 - (x * 0.05) ** 2 * (y * 0.1) ** 3 <= 0
               else ' ')
@@ -16,10 +16,10 @@ def generate_heart(text):
 st.title("Motif en forme de cœur personnalisé")
 
 # Entrée de l'utilisateur pour le texte à afficher
-user_input = st.text_input("Entrez le texte à afficher dans le cœur :", "YourName")
+user_input = st.text_input("Entrez le texte à afficher dans le cœur :", "Essai")
 
 # Génère le motif avec le texte fourni
 heart_text = generate_heart(user_input)
 
-# Affiche le motif
+# Affiche le motif avec un bloc de code pour un alignement plus stable
 st.code(heart_text, language="plaintext")
