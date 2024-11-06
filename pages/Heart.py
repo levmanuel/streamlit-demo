2aa20b63b0d7dd24fcde58df13446ec9d90df276
levmanuel <manuel.leveque@gmail.com>
2024-11-06 22:29:42 +0100

    OK

--- pages/Heart.py
+++ pages/Heart.py
@@ -4,11 +4,11 @@ def generate_heart(text):
     # Génère le motif en forme de cœur avec le texte fourni
     heart_pattern = '\n'.join(
         [''.join(
-            [(text[(x - y) % len(text)]
+            [(text[(x - y) % len(text)]  # Sélectionne le caractère en fonction de x et y
               if ((x * 0.05) ** 2 + (y * 0.1) ** 2 - 1) ** 3 - (x * 0.05) ** 2 * (y * 0.1) ** 3 <= 0
-              else ' ')
-             for x in range(-30, 30)])
-         for y in range(15, -15, -1)]
+              else ' ')  # Crée des espaces pour l'extérieur du cœur
+             for x in range(-30, 30)])  # Largeur du cœur
+         for y in range(15, -15, -1)]   # Hauteur du cœur
     )
     return heart_pattern
 
