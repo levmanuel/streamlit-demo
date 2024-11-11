f45bf92858c500c3f15f6106d9dd4d1109c12d0a
levmanuel <manuel.leveque@gmail.com>
2024-11-11 19:32:49 +0100

    OK

--- pages/checkbox_matchTemplate.py
+++ pages/checkbox_matchTemplate.py
@@ -6,14 +6,16 @@ from matplotlib import pyplot as plt
 st.title("Détection de cases à cocher")
 st.write("Chargez une image pour détecter les cases à cocher.")
 
-threshold = st.slider("thresold", 0, 100, 25, 2) / 100
+threshold = st.slider("thresold", 0, 100, 40, 2) / 100
 img_rgb = cv.imread('./assets/CHECK_TEST.png')
 img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
 template = cv.imread('./assets/CHECKED_ICON.png', cv.IMREAD_GRAYSCALE)
 w, h = template.shape[::-1]
 res = cv.matchTemplate(img_gray,template,cv.TM_CCOEFF_NORMED)
 loc = np.where( res >= threshold)
-for pt in zip(*loc[::-1]):
+points = zip(*loc[::-1])
+st.text(len(points))
+for pt in points:
     cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
 
 st.image(img_rgb, channels="BGR", caption="Image traitée")
