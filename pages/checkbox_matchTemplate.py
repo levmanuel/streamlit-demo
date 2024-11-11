0f5da2d8edde2de4b6991b87d6707139670287a9
levmanuel <manuel.leveque@gmail.com>
2024-11-11 19:30:46 +0100

    OK

--- pages/checkbox_matchTemplate.py
+++ pages/checkbox_matchTemplate.py
@@ -7,20 +7,14 @@ st.title("Détection de cases à cocher")
 st.write("Chargez une image pour détecter les cases à cocher.")
 
 threshold = st.slider("thresold", 0, 100, 25, 2) / 100
- 
 img_rgb = cv.imread('./assets/CHECK_TEST.png')
 img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
 template = cv.imread('./assets/CHECKED_ICON.png', cv.IMREAD_GRAYSCALE)
 w, h = template.shape[::-1]
 res = cv.matchTemplate(img_gray,template,cv.TM_CCOEFF_NORMED)
-# threshold = 0.8
 loc = np.where( res >= threshold)
 for pt in zip(*loc[::-1]):
     cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
- 
-# cv.imwrite('res.png',img_rgb)
-
-
 
 st.image(img_rgb, channels="BGR", caption="Image traitée")
 
