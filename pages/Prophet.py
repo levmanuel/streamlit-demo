90eeee55e207f81a0a35f133753a393e279c0476
levmanuel <manuel.leveque@gmail.com>
2025-01-01 22:11:23 +0100

    OK

--- pages/Prophet.py
+++ pages/Prophet.py
@@ -16,26 +16,23 @@ df = load_data()
 # Interface utilisateur
 st.title("Prévisions avec Prophet : Démonstration avec le dataset de Yosemite")
 
-# Sélection de la variable à prévoir
-target_column = df['y']
-
 # Configuration des paramètres du modèle
 periods = st.slider("Nombre de périodes à prévoir", min_value=1, max_value=365, value=30)
 changepoint_range = st.slider("Plage des points de changement", min_value=0.01, max_value=0.99, value=0.8)
 
 # Entraînement du modèle
 m = Prophet(changepoint_range=changepoint_range)
-m.fit(df[['ds', target_column]])
+m.fit(df[['ds', 'y']])
 future = m.make_future_dataframe(periods=periods)
 forecast = m.predict(future)
 
 # Visualisation des résultats
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=df['ds'], y=df[target_column], name='Données réelles'))
+fig.add_trace(go.Scatter(x=df['ds'], y=df['y'], name='Données réelles'))
 fig.add_trace(go.Scatter(x=forecast['ds'], y=forecast['yhat'], name='Prévisions'))
 fig.add_trace(go.Scatter(x=forecast['ds'], y=forecast['yhat_upper'], name='Intervalle de confiance supérieur', mode='lines', line=dict(color='rgba(0, 0, 255, 0.2)', dash='dash')))
 fig.add_trace(go.Scatter(x=forecast['ds'], y=forecast['yhat_lower'], name='Intervalle de confiance inférieur', mode='lines', line=dict(color='rgba(0, 0, 255, 0.2)', dash='dash')))
-fig.update_layout(title="Prévisions pour {}".format(target_column), xaxis_title="Date", yaxis_title=target_column)
+fig.update_layout(title="Prévisions pour {}".format('y'), xaxis_title="Date", yaxis_title='y')
 st.plotly_chart(fig)
 
 # Composantes du modèle
