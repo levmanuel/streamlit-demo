e228337eb191f189c4aa3b9289f964dc2b8934be
levmanuel <manuel.leveque@gmail.com>
2025-04-16 20:32:23 +0200

    ok

--- pages/gsheet.py
+++ pages/gsheet.py
@@ -6,6 +6,8 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 
 df = conn.read()
 
+st.write("Data from Google Sheets:")
+st.dataframe(df)
 # Print results.
 for row in df.itertuples():
     st.write(f"{row.name} has a :{row.pet}:")
