83b6d425fa743fc37a333a6866f722f461116890
levmanuel <manuel.leveque@gmail.com>
2025-04-16 22:26:27 +0200

    ok

--- pages/gsheet.py
+++ pages/gsheet.py
@@ -6,13 +6,15 @@ st.title("📄 Données Google Sheet")
 st.write("Cette page affiche les données d'une feuille Google Sheets et permet d'ajouter une nouvelle ligne.")
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
-df = conn.read()
+if "df" not in st.session_state:
+    st.session_state.df = conn.read(worksheet="Feuille 1")
 
 st.write("Data from Google Sheets:")
 if st.button("🔄 Mettre à jour les données"):
-    df = conn.read()
+    st.session_state.df = conn.read(worksheet="Feuille 1")
     st.success("Tableau mis à jour avec succès ✅")
-    st.dataframe(df)
+
+st.dataframe(st.session_state.df)
 
 # Ajouter une ligne via un formulaire
 with st.form("add_row_form"):
@@ -22,6 +24,6 @@ with st.form("add_row_form"):
 
     if submitted:
         new_row = pd.DataFrame([{"name": name, "pet": pet}])
-        df = pd.concat([df, new_row], ignore_index=True)
-        conn.update(worksheet="Feuille 1", data=df)
+        st.session_state.df = pd.concat([st.session_state.df, new_row], ignore_index=True)
+        conn.update(worksheet="Feuille 1", data=st.session_state.df)
         st.success("Ligne ajoutée avec succès ✅")
